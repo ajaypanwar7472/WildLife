a4ed3f225107fcc56fb7fdaf4ea594e521e04ce9
AJAY KUMAR <118057042+ajaypanwar7472@users.noreply.github.com>
2024-04-29 10:51:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//Navbar scroll
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar").classList.add("scrolled");
+    } else {
+        document.getElementById("navbar").classList.remove("scrolled");
+    }
+}
